e7ee0cbb30e63a7bc0072fec7b1a9a2de29c62df
dj49846917 <82104785@qq.com>
2019-08-20 23:22:02 +0800

    'echarts图形'

--- src/views/dashboard/startProgress.vue
+++ src/views/dashboard/startProgress.vue
@@ -22,85 +22,91 @@
 import { mapState } from 'vuex'
 import ECharts from 'vue-echarts'
 import 'echarts/lib/chart/line'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/legend'
 export default {
   components: {
     'v-chart': ECharts
   },
   computed: {
-	...mapState(['home']),
-	xData: function(){
-		if(JSON.stringify(this.$store.state.home.list)!== '{}'){
-			const arr = this.$store.state.home.list.totalNums
-			const x = []
-			arr.forEach(item=>{
-				x.push(item.tradeDate)
-			})
-			return x
-		}
-	},
-	yData: function(){
-		if(JSON.stringify(this.$store.state.home.list)!== '{}'){
-			const arr = this.$store.state.home.list.totalNums
-			const y = []
-			arr.forEach(item=>{
-				y.push(item.totalNum)
-			})
-			return y
-		}
-	},
+    ...mapState(['home']),
+    // eslint-disable-next-line vue/return-in-computed-property
+    xData: function() {
+      if (JSON.stringify(this.$store.state.home.list) !== '{}') {
+        const arr = this.$store.state.home.list.totalNums
+        const x = []
+        arr.forEach(item => {
+          x.push(item.tradeDate)
+        })
+        return x
+      }
+    },
+    // eslint-disable-next-line vue/return-in-computed-property
+    yData: function() {
+      if (JSON.stringify(this.$store.state.home.list) !== '{}') {
+        const arr = this.$store.state.home.list.totalNums
+        const y = []
+        arr.forEach(item => {
+          y.push(item.totalNum)
+        })
+        return y
+      }
+    }
   },
   data() {
     return {
       option: {
-		xAxis: {
-			type: 'category',
-			boundaryGap: false,
-			data: this.xData
-		  },
-		  yAxis: {
-			type: 'value',
-			splitLine:{
-			  show: true,
-			  lineStyle: {
-				type: 'dashed'
-			  }
-			}
-		  },
-		  tooltip: {
-			trigger: 'axis',
-			axisPointer: {
-			  type: 'cross',
-			  label: {
-				backgroundColor: '#5fa1e1'
-			  }
-			}
-		  },
-		  series: [{
-			data: this.yData,
-			type: 'line',
-			areaStyle: {
-			  color: '#d0e9ff'
-			},
-			lineStyle: {
-			  color: '#5fa1e1'
-			},
-			itemStyle: {
-			  color: '#5fa1e1'
-			},
-			smooth: true
-		  }]
-	  }
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: this.xData
+        },
+        yAxis: {
+          type: 'value',
+          splitLine: {
+            show: true,
+            lineStyle: {
+              type: 'dashed'
+            }
+          }
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#5fa1e1'
+            }
+          }
+        },
+        series: [
+          {
+            data: this.yData,
+            type: 'line',
+            areaStyle: {
+              color: '#d0e9ff'
+            },
+            lineStyle: {
+              color: '#5fa1e1'
+            },
+            itemStyle: {
+              color: '#5fa1e1'
+            },
+            smooth: true
+          }
+        ]
+      }
     }
   },
   watch: {
-	xData: function(newVal,oldVal){
-		console.log('222', newVal)
-		this.option.xAxis.data = newVal
-	},
-	yData: function(newVal,oldVal){
-		console.log('343', newVal)
-		this.option.series[0].data = newVal
-	}
+    xData: function(newVal, oldVal) {
+      console.log('222', newVal)
+      this.option.xAxis.data = newVal
+    },
+    yData: function(newVal, oldVal) {
+      console.log('343', newVal)
+      this.option.series[0].data = newVal
+    }
   }
 }
 </script>
@@ -108,16 +114,20 @@ export default {
 <style lang="scss" scoped>
 $textColor1: #666;
 $textColor2: #999;
-.progressBox_start_dicItem{
-    width: 50%;
-    color: $textColor2;
-    float: left;
-    .progressBox_start_dicItem_text2{
-        font-size: 24px;
-        color: $textColor1;
-        display: inline-block;
-        min-width: 60px;
-        text-align: right;
-    }
+.progressBox_start_dicItem {
+  width: 50%;
+  color: $textColor2;
+  float: left;
+  .progressBox_start_dicItem_text2 {
+    font-size: 24px;
+    color: $textColor1;
+    display: inline-block;
+    min-width: 60px;
+    text-align: right;
+  }
+}
+.echarts{
+	left: -14px;
+  top: -22px;
 }
 </style>
